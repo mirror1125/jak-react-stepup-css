380585b076113866722b00f602ff5fa7bc10feb7
mirror1125 <mirrorseek@gmail.com>
2022-07-29 19:42:13 +0900

    スタイル割当処理の実装

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+};
